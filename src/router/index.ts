ea8e73cc2497c613ab45cab067e963a77ac1998a
codeyusan <codeyusan@163.com>
2023-07-06 16:12:59 +0800

    hash router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router
 import Home from '../views/Home.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  // history: createWebHashHistory(),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
